5ddaea04f743c9837650483a4c7b293e76859c0f
Rhyn <tonymigeonpro@gmail.com>
2022-02-06 19:08:02 +0100

    Tests

---
+++ src/components/CharacterCreation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CharacterCreation = () => {
+
+    return(
+        <div className="characterCreationContainer">
+            <form>
+                Test
+            </form>
+        </div>
+    )
+
+}
+
+export default CharacterCreation;
