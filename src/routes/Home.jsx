ca773965c662ac518d3dae67e9dbca50602919fe
Rhyn <tonymigeonpro@gmail.com>
2022-02-06 19:06:50 +0100

    Temporary Grid

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import Header from "../components/Header";
+import Daily from "../components/Daily";
+import AddCharacter from "../components/AddCharacter";
+import Grid from "@mui/material/Grid";
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <header>
+      <div className="homeContainer">
+        <Header />
+        <div className="bodyPage">
+          <AddCharacter />
+          <Grid container
+                justifyContent="center" >
+            <Grid item md={2} xs={12} sm={6}>
+              <Daily />
+            </Grid>
+            <Grid item md={2} xs={12} sm={6}>
+              <Daily />
+            </Grid>
+            <Grid item md={2} xs={12} sm={6}>
+              <Daily />
+            </Grid>
+            <Grid item md={2} xs={12} sm={6}>
+              <Daily />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </header>
   );
 };
 
