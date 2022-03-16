4dc4ba8844099935fa875274d72eb358b375b4fd
Rhyn <rhyyn.lol@gmail.com>
2022-03-16 12:50:54 +0100

    Merchant tracker v2

--- src/components/CountdownCompo.jsx
+++ src/components/CountdownCompo.jsx
@@ -2,21 +2,55 @@ import React, { useEffect, useState } from "react";
 import CountdownTimer from "./CountdownTimer";
 
 const CountdownCompo = (props) => {
-  let data = props.merchantSchedule.times;
+  const data = props.merchantSchedule.times; // Spawn Times
+  const [showCountDown, setShowCountDown] = useState(false);
+  const hoursArray = [];
+  const minutesArray = [];
+  const currentHour = new Date().getHours();
+
+
+  // Convert Merchant hour spawn from AM to PM if currentHour is between 12 and 23 then Push to new hoursArray
+  data.map((item, index) => {
+    if (currentHour >= 12) {
+      hoursArray.push(parseInt(item.hour) + 12);
+      minutesArray.push(item.minute)
+    } else {
+      hoursArray.push(item.hour);
+      minutesArray.push(item.minute)
+    }
+  });
+
+  // Find hour that is the closest to currentHour clockwise
+  const hourFound = hoursArray.find(function(element) {return element > currentHour});  
+
+  // Previous way of finding closest hour but not clockwise
+  // const output = hoursArray.reduce((prev, curr) =>
+  //   Math.abs(curr - currentHour) < Math.abs(prev - currentHour) ? curr : prev
+  // );
+
 
   return (
     <div className="merchantCard">
       <p>Name: {props.merchantName}</p>
       <p>Location: {props.merchantLocation}</p>
-      <button>Track this Merchant</button>
+      <button
+        id={props.merchantName}
+        onClick={() => setShowCountDown(!showCountDown)}
+      >
+        Track this Merchant
+      </button>
+      {showCountDown ? <CountdownTimer hour={hourFound}></CountdownTimer> : null}
       {data &&
         data.map((item, index) => {
-          return (<ul>
-            <li>{item.hour}:{item.minute}</li>
-          </ul>)
+          return (
+            <li key={index}>
+              {item.hour}:{item.minute}
+            </li>
+          );
         })}
     </div>
   );
 };
 
 export default CountdownCompo;
+
--- src/routes/Merchant.jsx
+++ src/routes/Merchant.jsx
@@ -6,21 +6,6 @@ import MerchantList from "../MerchantsList.json";
 const Merchant = () => {
   // console.log(MerchantList.merchants01[0].spawntimer.times.spawn01);
 
-  // MerchantList.merchants01.map((item, index) => (
-  //   console.log(item.spawntimer.times)))
-
-
-  // MerchantList.merchants01[0].spawntimer.times.map((item,index) => {
-  //   console.log(item);
-  // })
-  
-  // console.log(MerchantList.merchants01[0].spawntimer.times);
-
-  ///MAPPPPPPPPP
-  let data = MerchantList.merchants01[0].spawntimer.times;
-  data.forEach(element => {
-    // console.log(element.hour);
-  });
 
 
   return (
@@ -29,8 +14,9 @@ const Merchant = () => {
         <Header></Header>
       </header>
       <div className="merchantBody">
+      {/* <img src="/images/Mac/traveling_merchant_mac_delphi_township.jpg"/> */}
         <div className="merchantGroup">
-        <h2 className="merchantGroupTitle">Schedule 1</h2>
+          <h2 className="merchantGroupTitle">Schedule 1</h2>
           {MerchantList.merchants01.map((item, index) => (
             <CountdownCompo
               key={index}
@@ -41,7 +27,7 @@ const Merchant = () => {
           ))}
         </div>
         <div className="merchantGroup">
-        <h2 className="merchantGroupTitle">Schedule 2</h2>
+          <h2 className="merchantGroupTitle">Schedule 2</h2>
           {MerchantList.merchants02.map((item, index) => (
             <CountdownCompo
               key={index}
@@ -52,7 +38,7 @@ const Merchant = () => {
           ))}
         </div>
         <div className="merchantGroup">
-        <h2 className="merchantGroupTitle">Schedule 3</h2>
+          <h2 className="merchantGroupTitle">Schedule 3</h2>
           {MerchantList.merchants03.map((item, index) => (
             <CountdownCompo
               key={index}
@@ -69,10 +55,4 @@ const Merchant = () => {
 
 export default Merchant;
 
-// logic :
-// MerchantList.merchants01[0].spawntimer.times.forEach((item) => {
-//      now = currentTime
-//      remainingTime =
-//
-//})
-//
+
