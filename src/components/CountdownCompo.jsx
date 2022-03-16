c1e6c42b57334009cc8664c84aaea44d473dfc51
Rhyn <rhyyn.lol@gmail.com>
2022-03-16 16:42:34 +0100

    Merchant Tracker v3

--- src/components/CountdownCompo.jsx
+++ src/components/CountdownCompo.jsx
@@ -2,55 +2,57 @@ import React, { useEffect, useState } from "react";
 import CountdownTimer from "./CountdownTimer";
 
 const CountdownCompo = (props) => {
-  const data = props.merchantSchedule.times; // Spawn Times
-  const [showCountDown, setShowCountDown] = useState(false);
+  const data = props.wantedMerchant.spawntimer.times; //Spawn Times
+  const [showSpawnTable, setShowSpawntable] = useState(false);
   const hoursArray = [];
   const minutesArray = [];
   const currentHour = new Date().getHours();
 
-
   // Convert Merchant hour spawn from AM to PM if currentHour is between 12 and 23 then Push to new hoursArray
-  data.map((item, index) => {
-    if (currentHour >= 12) {
-      hoursArray.push(parseInt(item.hour) + 12);
-      minutesArray.push(item.minute)
-    } else {
-      hoursArray.push(item.hour);
-      minutesArray.push(item.minute)
-    }
-  });
+  data &&
+    data.map((item, index) => {
+      if (currentHour >= 12) {
+        hoursArray.push(parseInt(item.hour) + 12);
+        minutesArray.push(item.minute);
+      } else {
+        hoursArray.push(item.hour);
+        minutesArray.push(item.minute);
+      }
+    });
 
-  // Find hour that is the closest to currentHour clockwise
-  const hourFound = hoursArray.find(function(element) {return element > currentHour});  
+  // // Find hour that is the closest to currentHour clockwise
+  const hourFound = hoursArray.find(function (element) {
+    return element > currentHour;
+  });
 
   // Previous way of finding closest hour but not clockwise
   // const output = hoursArray.reduce((prev, curr) =>
   //   Math.abs(curr - currentHour) < Math.abs(prev - currentHour) ? curr : prev
   // );
 
-
   return (
     <div className="merchantCard">
-      <p>Name: {props.merchantName}</p>
-      <p>Location: {props.merchantLocation}</p>
+      <p>{props.wantedMerchant.name}</p>
+      <p>Name: {props.wantedMerchant.name}</p>
+      <p>Location: {props.wantedMerchant.location}</p>
+      <CountdownTimer hour={hourFound}></CountdownTimer>
       <button
-        id={props.merchantName}
-        onClick={() => setShowCountDown(!showCountDown)}
+        className={props.wantedMerchant.name}
+        onClick={() => setShowSpawntable(!showSpawnTable)}
       >
-        Track this Merchant
+        Show SpawnTable for this Merchant
       </button>
-      {showCountDown ? <CountdownTimer hour={hourFound}></CountdownTimer> : null}
-      {data &&
-        data.map((item, index) => {
-          return (
-            <li key={index}>
-              {item.hour}:{item.minute}
-            </li>
-          );
-        })}
+      {showSpawnTable
+        ? data.map((item, index) => {
+            return (
+              <p key={index}>
+                {item.hour}:{item.minute}
+              </p>
+            );
+          })
+        : null}
     </div>
   );
 };
 
 export default CountdownCompo;
-
--- src/routes/Merchant.jsx
+++ src/routes/Merchant.jsx
@@ -1,53 +1,59 @@
-import React from "react";
+import { React, useState } from "react";
 import Header from "../components/Header";
 import CountdownCompo from "../components/CountdownCompo";
 import MerchantList from "../MerchantsList.json";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const Merchant = () => {
-  // console.log(MerchantList.merchants01[0].spawntimer.times.spawn01);
+  const merchants = MerchantList.merchants;
+  const [wantedMerchant, setWantedMerchant] = useState([]);
+  const [components, setComponents] = useState();
 
+  const onClick = (item) => {
+    //                        Add Merchant user chosed to State to pass down to CountdownCompo
+    setWantedMerchant((prevWantedMerchant) => [...prevWantedMerchant, item]);
+  };
 
-
+  //{/* <img src="/images/Mac/traveling_merchant_mac_delphi_township.jpg"/> */}
   return (
     <div className="merchantPage">
       <header>
         <Header></Header>
       </header>
       <div className="merchantBody">
-      {/* <img src="/images/Mac/traveling_merchant_mac_delphi_township.jpg"/> */}
-        <div className="merchantGroup">
-          <h2 className="merchantGroupTitle">Schedule 1</h2>
-          {MerchantList.merchants01.map((item, index) => (
-            <CountdownCompo
-              key={index}
-              merchantName={item.name}
-              merchantLocation={item.location}
-              merchantSchedule={item.spawntimer}
-            ></CountdownCompo>
-          ))}
-        </div>
-        <div className="merchantGroup">
-          <h2 className="merchantGroupTitle">Schedule 2</h2>
-          {MerchantList.merchants02.map((item, index) => (
-            <CountdownCompo
-              key={index}
-              merchantName={item.name}
-              merchantLocation={item.location}
-              merchantSchedule={item.spawntimer}
-            ></CountdownCompo>
-          ))}
-        </div>
-        <div className="merchantGroup">
-          <h2 className="merchantGroupTitle">Schedule 3</h2>
-          {MerchantList.merchants03.map((item, index) => (
-            <CountdownCompo
-              key={index}
-              merchantName={item.name}
-              merchantLocation={item.location}
-              merchantSchedule={item.spawntimer}
-            ></CountdownCompo>
-          ))}
-        </div>
+        <DropdownButton
+          variant="primary"
+          id="dropdown-basic-button"
+          title="Select Merchants to Track"
+          autoClose="inside"
+          className="test"
+        >
+          {merchants.map((item, index) => {
+            //          Map through the list of merchants of user to see
+            return (
+              <Dropdown.Item key={index}>
+                <h4
+                  onClick={() => {
+                    //                     onClick call onClick function and set item to State
+                    onClick(item);
+                  }}
+                >
+                  {item.name}
+                </h4>
+              </Dropdown.Item>
+            );
+          })}
+        </DropdownButton>
+        {wantedMerchant && //                           If wantedMerchant exists
+          wantedMerchant.map((item, index) => {
+            //      map through then return each merchant user selected
+            return (
+              <CountdownCompo
+                key={index}
+                wantedMerchant={item}
+              ></CountdownCompo>
+            );
+          })}
       </div>
     </div>
   );
@@ -55,4 +61,40 @@ const Merchant = () => {
 
 export default Merchant;
 
+// PREVIOUS VERSION OF MAP
 
+{
+  /* <div className="merchantGroup">
+<h2 className="merchantGroupTitle">Schedule 1</h2>
+{MerchantList.merchants01.map((item, index) => (
+  <CountdownCompo
+    key={index}
+    merchantName={item.name}
+    merchantLocation={item.location}
+    merchantSchedule={item.spawntimer}
+  ></CountdownCompo>
+))}
+</div>
+<div className="merchantGroup">
+<h2 className="merchantGroupTitle">Schedule 2</h2>
+{MerchantList.merchants02.map((item, index) => (
+  <CountdownCompo
+    key={index}
+    merchantName={item.name}
+    merchantLocation={item.location}
+    merchantSchedule={item.spawntimer}
+  ></CountdownCompo>
+))}
+</div>
+<div className="merchantGroup">
+<h2 className="merchantGroupTitle">Schedule 3</h2>
+{MerchantList.merchants03.map((item, index) => (
+  <CountdownCompo
+    key={index}
+    merchantName={item.name}
+    merchantLocation={item.location}
+    merchantSchedule={item.spawntimer}
+  ></CountdownCompo>
+))}
+</div> */
+}
