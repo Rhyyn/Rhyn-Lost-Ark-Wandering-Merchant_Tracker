adff6d122afbe066a7b18fc0347fcf0ebf59036e
Rhyn <rhyyn.lol@gmail.com>
2022-04-05 17:42:19 +0200

    Route Home is now Merchant

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./routes/Home";
 import Merchant from "./routes/Merchant";
 import Todo from "./routes/ToDo";
 
@@ -10,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Merchant />}></Route>
           <Route path="/merchant" element={<Merchant />}></Route>
           <Route path="/todo" element={<Todo />}></Route>
         </Routes>
