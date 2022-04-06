ef3dca935d0a3f17e3c97dc65e4f43a529205aab
Rhyn <rhyyn.lol@gmail.com>
2022-04-06 18:06:10 +0200

    Refactored Dropdown Button

--- src/routes/Merchant.jsx
+++ src/routes/Merchant.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import CountdownCompo from "../components/CountdownCompo";
 import MerchantList from "../MerchantsList.json";
 import { DropdownButton, Dropdown } from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
 
 const Merchant = () => {
   const merchants = MerchantList.merchants;
@@ -23,29 +24,71 @@ const Merchant = () => {
           variant="primary"
           id="dropdown-basic-button"
           title="Select Merchants to Track"
-          autoClose="inside"
+          autoClose="true"
           className="dropdownButton"
         >
-          {merchants.map((item, index) => {
-            //          Map through the list of merchants of user to see
-            return (
-              <Dropdown.Item key={index}>
-                <h4
-                  onClick={() => {
-                    //                     onClick call onClick function and set item to State
-                    onClick(item);
-                  }}
-                >
-                  {item.name}
-                </h4>
-              </Dropdown.Item>
-            );
+        <Dropdown.Divider />
+          <h5 className="dropdownSchedules">Schedule 1</h5>
+          <Dropdown.Divider />
+          {merchants.map((item,index) => {
+            if (item.group === "1") {
+              return (
+                <Dropdown.Item key={index}>
+                  <h5
+                    onClick={() => {
+                      //                     onClick call onClick function and set item to State
+                      onClick(item);
+                    }}
+                  >
+                    {item.name}
+                  </h5>
+                </Dropdown.Item>
+              );
+            }
+          })}
+          <Dropdown.Divider />
+          <h5 className="dropdownSchedules">Schedule 2</h5>
+          <Dropdown.Divider />
+          {merchants.map((item,index) => {
+            if (item.group === "2") {
+              return (
+                <Dropdown.Item key={index}>
+                  <h5
+                    onClick={() => {
+                      //                                  onClick call onClick function and set item to State
+                      onClick(item);
+                    }}
+                  >
+                    {item.name}
+                  </h5>
+                </Dropdown.Item>
+              );
+            }
+          })}
+          <Dropdown.Divider />
+          <h5 className="dropdownSchedules">Schedule 3</h5>
+          <Dropdown.Divider />
+          {merchants.map((item,index) => {
+            if (item.group === "3") {
+              return (
+                <Dropdown.Item key={index}>
+                  <h5
+                    onClick={() => {
+                      //                                  onClick call onClick function and set item to State
+                      onClick(item);
+                    }}
+                  >
+                    {item.name}
+                  </h5>
+                </Dropdown.Item>
+              );
+            }
           })}
         </DropdownButton>
         <div className="merchantList">
           {wantedMerchant && //                           If wantedMerchant exists
             wantedMerchant.map((item, index) => {
-              //      map through then return each merchant user selected
+              //                                           map through then return each merchant user selected
               return (
                 <CountdownCompo
                   id={index}
