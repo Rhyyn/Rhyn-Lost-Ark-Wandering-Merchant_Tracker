20b82b7a400915aebe21fbcdd0671d523b4a2d11
Tony Migeon <rhyyn.lol@gmail.com>
2022-05-29 18:59:47 +0200

    Fix: Alt property on img tag missing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="headerBox">
             <h1 className="brandName">Lost Ark Merchant Tracker</h1>
-            <img className="brandLogo" src="/images/mokokoLogo.png" />
+            <img className="brandLogo" src="/images/mokokoLogo.png" alt="mokokoLogo image" />
         </div>
     );
 };
